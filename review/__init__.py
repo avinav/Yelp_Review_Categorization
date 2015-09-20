3e56b3dde39391e9f7ab984b1f589c4b418c85c9
avinav <sharan.avinav@gmail.com>
2015-09-20 10:13:50 -0400

    updated

--- review/__init__.py
+++ review/__init__.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 from JsonToDF import get_data
+import pickle
+import os
 
-count = 100
+count = 2
 reviews_df = get_data(count)
+if os.path.isfile('data.p'):
+    data = pickle.load(open('data.p','r'))
