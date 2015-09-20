d3432e764b49b59dec66f4fd8471699beedee790
avinav <sharan.avinav@gmail.com>
2015-09-20 09:54:35 -0400

    view update

--- server/app/views.py
+++ server/app/views.py
@@ -4,21 +4,32 @@ from review.review_score import get_reviews
 from review import reviews_df
 from business import get_business_score
 import numpy as np
+import math
 
 @app.route('/', methods = ['GET','POST'])
 @app.route('/index', methods = ['GET','POST'])
 def index():
     #bus_id = "vcNAWiLM4dR7D2nwwJ7nCA"
-    bus_id = "mVHrayjG3uZ_RLHkLj-AMg"
+    #bus_id = "mVHrayjG3uZ_RLHkLj-AMg"
+    bus_id = "1qCuOcks5HRv67OHovAVpg"
     res = get_reviews(bus_id)
-    cat_score  = get_business_score(res)
-    name = np.unique(reviews_df[reviews_df.business_id == bus_id].name)[0]
-    print res
-    return render_template("index.html",
-                           title = "Yelp Review Categorization",
-                           cat_score = cat_score,
-                           res = res,
-                           name = name)
+    try:
+        cat_score  = get_business_score(res)
+        name = np.unique(reviews_df[reviews_df.business_id == bus_id].name)[0]
+        print res
+        return render_template("index.html",
+                               title = "Yelp Review Categorization",
+                               cat_score = cat_score,
+                               res = res,
+                               name = name)
+    except :
+        print "nan",res
+        return render_template("index.html",
+        title = "Yelp Review Categorization",
+        cat_score = {},
+        res = [],
+        name = " ")
+
 
 @app.route('/all', methods = ['GET', 'POST'])
 def all_restaurants():
@@ -28,10 +39,14 @@ def all_restaurants():
         _dict = {}
         _dict['id'] = _id
         _dict['name'] = np.unique(reviews_df[reviews_df.business_id == _id].name)[0]
+        print _id
         reviews = get_reviews(_id)
-        cat_score = get_business_score(reviews)
-        _dict['cat_score'] = cat_score
-        data.append(_dict)
+        try:
+            cat_score = get_business_score(reviews)
+            _dict['cat_score'] = cat_score
+            data.append(_dict)
+        except:
+            pass
     return render_template("all_retaurants",
                             title = "Yelp Review Categorization",
                             data = data)
