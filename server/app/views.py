00b8ea2e9ff38fb231423e24a10d646670363577
avinav <sharan.avinav@gmail.com>
2015-09-20 09:06:08 -0400

    all files

--- server/app/views.py
+++ server/app/views.py
@@ -19,4 +19,20 @@ def index():
                            cat_score = cat_score,
                            res = res,
                            name = name)
+i
+@app.route('/all',method = ['GET', 'POST'])
+def all_restaurants():
+    id_list = np.unique(reviews_df.id)
+    data = []
+    for _id in id_list:
+        _dict = {}
+        _dict['id'] = _id
+        _dict['name'] = np.unique(reviews_df[reviews_df.business_id == _id])[0].name
+        reviews = get_reviews(_id)
+        cat_score = get_business_score(reviews)
+        _dict['cat_score'] = cat_score
+        data.append(_dict)
+    return render_template("all_retaurants",
+                            title = "Yelp Review Categorization",
+                            data = data)
 
