b024401ed2d01a36d4154f6600ebb0994769ffd9
zstring <jai.himanshu001@gmail.com>
2015-09-20 09:35:10 -0400

    only call similatir

--- business.py
+++ business.py
@@ -7,10 +7,10 @@ def get_business_score(reviews):
     final_cat = {"food":0, "service":0,"ambiance":0, "money":0}
     cat_count = {"food":1, "service":1, "ambiance":1, "money":1}
     for sent in sentences:
-        cat = sim.get_similarity(sent)
-        sent = senti.get_sentiment(sent)
-        if sent != 0.5:
-		final_cat[cat] = sent + final_cat[cat]
+        sentr = senti.get_sentiment(sent)
+        if sentr != 0.5:
+            cat = sim.get_similarity(sent)
+            final_cat[cat] = sentr + final_cat[cat]
         	cat_count[cat] = 1 + cat_count[cat]        
     for key in final_cat.keys():
             final_cat[key] = final_cat[key] / (1.0 * cat_count[key])
