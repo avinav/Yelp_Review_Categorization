d9e44449797138bfbef6a0ae643cda9b595d7047
avinav <sharan.avinav@gmail.com>
2015-09-20 00:38:52 -0400

    dataframe with business category (restaurant)

--- JsonToDF.py
+++ JsonToDF.py
@@ -1,9 +1,6 @@
-
-from urllib2 import Request, urlopen
 import pandas as pd
 import json
 from pandas.io.json import json_normalize
-from io import StringIO
 
 #with open('/home/zstring/Dropbox/UB/GDG/yelp_dataset_challenge_academic_dataset/smalldataset') as data_file:
 #	data = json.load(data_file)
@@ -15,34 +12,63 @@ from io import StringIO
 
 
 #file = '/home/zstring/Dropbox/UB/GDG/yelp_dataset_challenge_academic_dataset/smalldataset'
-file = '/home/zstring/Dropbox/UB/GDG/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
-df = None
-count = 0
-TOTAL_COUNT = 100
-with open(file) as f:
-    for line in f:
-        while True:
-            try:
-            	#print "%%%% lines",  line
-                jfile = json.loads(line)
+def get_data(count):
+    file = '/home/avinav/Dropbox/git/Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
+    df = None
+    count = 0
+    TOTAL_COUNT = count
+    with open(file) as f:
+        for line in f:
+            while True:
+                try:
+                	#print "%%%% lines",  line
+                    jfile = json.loads(line)
+                    break
+                except ValueError:
+                    # Not yet a complete JSON value
+                    line += next(f)
+                    #print "@#@@@@@@ after, ", line
+            data_norm = json_normalize(jfile)
+            #print "converting to df", data_norm
+            if df is None:
+                #print "df is None"
+                df = pd.DataFrame(data_norm)  
+            else:
+                #print "df has something"
+                df = df.append(pd.DataFrame(data_norm))
+            #print "Shape, ", df.shape
+            count = count + 1
+            if count > 100:
                 break
-            except ValueError:
-                # Not yet a complete JSON value
-                line += next(f)
-                #print "@#@@@@@@ after, ", line
-        data_norm = json_normalize(jfile)
-        #print "converting to df", data_norm
-    	if df is None:
-      		#print "df is None"
-      		df = pd.DataFrame(data_norm)  
-        else:
-        	#print "df has something"
-        	df = df.append(pd.DataFrame(data_norm))
-        #print "Shape, ", df.shape
-        count = count + 1
-    	if count > 100:
-    		break
-
-
-
-df.shape
+    df['restaurant'] = ['Restaurants' in category for category in df.categories]
+    
+    file = '/home/avinav/Dropbox/git/Yelp/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
+    df_review = None
+    count = 0
+    with open(file) as f:
+        for line in f:
+            while True:
+                try:
+                    #print "%%%% lines",  line
+                    jfile = json.loads(line)
+                    break
+                except ValueError:
+                    # Not yet a complete JSON value
+                    line += next(f)
+                    #print "@#@@@@@@ after, ", line
+            data_norm = json_normalize(jfile)
+            #print "converting to df", data_norm
+            if df_review is None:
+                  #print "df is None"
+                  df_review = pd.DataFrame(data_norm)  
+            else:
+                #print "df has something"
+                df_review = df_review.append(pd.DataFrame(data_norm))
+            #print "Shape, ", df.shape
+            count = count + 1
+            if count > 100:
+                break
+    
+    df = pd.merge(df_review, df, on='business_id', how='outer')
+    
+    return df
---
+++ Runner.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from JsonToDF import get_data
+
+count = 100
+df = get_data(count)
+print df
+
