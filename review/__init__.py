7f62deb1cfe9fb4a9683eb11dabd5a81529e7a68
avinav <sharan.avinav@gmail.com>
2015-09-20 02:16:34 -0400

    get_reviews function

---
+++ review/__init__.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from JsonToDF import get_data
+
+count = 100
+reviews_df = get_data(count)
---
+++ review/review_score.py
@@ -0,0 +1,6 @@
+from review import reviews_df
+
+# accepts id hash and returns Series object
+def get_reviews(bus_id):
+    return reviews_df[reviews_df.business_id == bus_id].text
+    
